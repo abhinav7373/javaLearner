d47cc7b6d3c1eecf0a044f9834cad2426d3c65f1
akash gupta <105080627+akg91@users.noreply.github.com>
2022-09-17 10:23:35 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("learn java using intellij idea now");
         System.out.println("trying for checking");
+        System.out.println("trying again for checking");
     }
 }
