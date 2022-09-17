c562a5cdf37fa9edf159f6aac9fc73d7f11ce572
akg91 <developerakash425@gmail.com>
2022-09-17 10:21:11 +0530

    java1 commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("learn java using intellij idea now");
+        System.out.println("trying for checking");
     }
 }
